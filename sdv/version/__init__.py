418036bb6849f5b8cde60435d62041df533d9f0e
Gaurav Sheni <gvsheni@gmail.com>
2024-07-22 22:29:48 -0400

    Update __init__.py

--- sdv/version/__init__.py
+++ sdv/version/__init__.py
@@ -4,5 +4,6 @@ from importlib.metadata import version
 
 public = version('sdv')
 enterprise = None
+connectors = None
 
-__all__ = ('public', 'enterprise')
+__all__ = ('public', 'enterprise', 'connectors')
